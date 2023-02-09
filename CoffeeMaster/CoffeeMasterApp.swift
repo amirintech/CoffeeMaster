47e86883a888dee6041b3f52b22139b801864ab4
amirintech <amir224030@bue.edu.eg>
2023-02-09 11:48:05 +0200

    Initial Commit

---
+++ CoffeeMaster/CoffeeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMasterApp.swift
+//  CoffeeMaster
+//
+//  Created by Amir on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
